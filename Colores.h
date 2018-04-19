4ec70f85b3357c3a799393e1a87c7bb184b1ebe4
nachozullo <ignaciozullo@gmail.com>
2018-04-19 17:25:01 -0300

    Libreria de colores y cambios en la consola

---
+++ Colores.h
@@ -0,0 +1,21 @@
+#ifndef COLORES_H_
+#define COLORES_H_
+
+#define ENTER "\n\n\n"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_BOLDBLACK   "\033[1m\033[30m"
+#define ANSI_COLOR_BOLDRED     "\033[1m\033[31m"
+#define ANSI_COLOR_BOLDGREEN   "\033[1m\033[32m"
+#define ANSI_COLOR_BOLDYELLOW  "\033[1m\033[33m"
+#define ANSI_COLOR_BOLDBLUE    "\033[1m\033[34m"
+#define ANSI_COLOR_BOLDMAGENTA "\033[1m\033[35m"
+#define ANSI_COLOR_BOLDCYAN    "\033[1m\033[36m"
+#define ANSI_COLOR_BOLDWHITE   "\033[1m\033[37m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#endif /* COLORES_H_ */
