82243231f0bbecda9b2f5501584a10b700d1e47d
nachozullo <ignaciozullo@gmail.com>
2018-04-20 12:33:46 -0300

    Cambios en la consola

---
+++ ConsolaPlanificador/src/consola.h
@@ -0,0 +1,31 @@
+/*
+ * consola.h
+ *
+ *  Created on: 20 abr. 2018
+ *      Author: nacho
+ */
+
+#ifndef CONSOLAPLANIFICADOR_SRC_CONSOLA_H_
+#define CONSOLAPLANIFICADOR_SRC_CONSOLA_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <commons/string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../../Colores.h"
+
+int com_pausar(char **args);
+int com_continuar(char **args);
+int com_bloquear(char **args);
+int com_desbloquear(char **args);
+int com_listar(char **args);
+int com_kill(char **args);
+int com_status(char **args);
+int com_deadlock(char **args);
+
+int ejecutar_linea(char **args);
+
+#endif /* CONSOLAPLANIFICADOR_SRC_CONSOLA_H_ */
