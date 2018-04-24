ef53100347057ce6b325095e475e39470b5068a9
nachozullo <ignaciozullo@gmail.com>
2018-04-24 11:19:20 -0300

    Agrego unas librerias que faltaban

--- ConsolaPlanificador/src/consola.h
+++ ConsolaPlanificador/src/consola.h
@@ -12,10 +12,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <commons/log.h>
 #include <commons/string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
 #include "../../Colores.h"
+
+#define IP "127.0.0.1"
+#define PUERTO "6666"
+
+t_log* logger;
 
 int com_pausar(char **args);
 int com_continuar(char **args);
