42fcc6a85a0323bde1f8d205e13447b46631a40f
nachozullo <ignaciozullo@gmail.com>
2018-06-05 01:02:18 -0300

    Ordeno un poco el codigo

--- Coordinador/coordinador.h
+++ Coordinador/coordinador.h
@@ -16,29 +16,35 @@
 char* PUERTO;
 #define NUMEROCLIENTES 20
 #define CANTIDADCLAVES 20
+#define TAMANIOMAXIMOCLAVE 40
 
 typedef struct Cliente{
 	char nombre[14];
 	int fd; 				//ESTRUCTURA PARA RECONOCER A LOS ESI Y DEMAS CLIENTES
 	int identificadorESI;
 	int identificadorInstancia;
-}cliente;
+}cliente_t;
 
-struct arg_struct {
+typedef struct arg_struct {
 	int socket;
-	cliente socketCliente;
-};
+	cliente_t socketCliente;
+}arg_t;
 
 typedef struct arg_esi {
 	int socketPlanificador;
-	cliente* socketCliente;
+	cliente_t* socketCliente;
 }arg_esi_t;
+
+typedef struct clave{
+	char* clave;
+	int instancia;
+}clave_t;
 
 t_log* logger;
 t_log* logOperaciones;
 t_config* config;
-cliente socketCliente[NUMEROCLIENTES];
-char* clavesExistentes[CANTIDADCLAVES];
+cliente_t socketCliente[NUMEROCLIENTES];
+clave_t clavesExistentes[CANTIDADCLAVES];
 
 /*FUNCIONES DE CONEXION */
 void _exit_with_error(int socket, char* mensaje);
@@ -49,7 +55,7 @@ int conectarSocketYReservarPuerto();
 void escuchar(int socket);
 
 /* Asigna nombre a cada cliente particular:  Instancia, ESI, Planificador */
-void asignarNombreAlSocketCliente(struct Cliente* socketCliente, char* nombre);
+void asignarNombreAlSocketCliente(cliente_t* socketCliente, char* nombre);
 
 
 void enviarMensaje(int socketCliente, char* msg);
@@ -57,20 +63,20 @@ void enviarMensaje(int socketCliente, char* msg);
 /* Recibe sentencia del ESI */
 void recibirSentenciaESI(void* socketCliente);
 
-void recibirIDdeESI(cliente* cliente);
+void recibirIDdeESI(cliente_t* cliente);
 
 /* Asigna ID a cada Instancia para identificarlas */
-void asignarIDdeInstancia(struct Cliente* socketCliente, int id);
+void asignarIDdeInstancia(cliente_t* socketCliente, int id);
 
 void recibirMensaje(void* argumentos);
 
 /* CREACION DE HILOS PARA CADA CLIENTE */
 
-void crearHiloPlanificador(cliente socketCliente);
+void crearHiloPlanificador(cliente_t socketCliente);
 
-void crearHiloInstancia(cliente socketCliente);
+void crearHiloInstancia(cliente_t socketCliente);
 
-void crearHiloESI(cliente* socketCliente, int socketPlanificador);
+void crearHiloESI(cliente_t* socketCliente, int socketPlanificador);
 
 /* Crea un array con las instancias que se encuentran conectadas y muestra la cantidad*/
 void instanciasConectadas();
@@ -88,7 +94,7 @@ void enviarSentenciaESI(int socket, header_t* header, char* clave, char* valor);
 void enviarSentenciaAPlanificador(int socket, header_t* header, char* clave, int idESI);
 
 /* Maneja todos los clientes que se pueden conectar */
-void aceptarCliente(int socket, cliente* socketCliente);
+void aceptarCliente(int socket, cliente_t* socketCliente);
 int envioHandshake(int socketCliente) ;
 
 /* Identifica si se conecto ESI, PLANIFICADOR o INSTANCIA */
@@ -96,7 +102,7 @@ int reciboIdentificacion(int socketCliente);
 
 void intHandler();
 
-void tratarSegunOperacion(header_t* header, cliente* socket, int socketPlanificador);
+void tratarSegunOperacion(header_t* header, cliente_t* socket, int socketPlanificador);
 
 void actualizarVectorInstanciasConectadas();
 
