10e3c8b173d8c3ffc1c5796b12957d4ab628b0d6
Agustin Moles <agusmoles@gmail.com>
2018-06-23 20:28:58 -0300

    Revert "Nada importante. Codeando con nachiii"
    This reverts commit 7a12b0d1c28cdd7a6877acbd8609e23a10ed02c2.

--- Instancias/instancia.c
+++ Instancias/instancia.c
@@ -30,16 +30,7 @@ void setearConfigEnVariables() {
 
     storageFijo = malloc(CANTIDADENTRADAS * TAMANIOENTRADA); // CREO VECTOR
 
-    inicializarStorage();
-
     storage = storageFijo; 			// SIEMPRE TOCAR EL STORAGE AUXILIAR (STORAGE), EL OTRO NO
-}
-
-void inicializarStorage() {
-	for (int i=0; i<CANTIDADENTRADAS; i++) {
-		storage = buscarEnStorage(i);
-		strcpy(storage, "");
-	}
 }
 
 int conectarSocket() {
@@ -131,18 +122,17 @@ void recibirInstruccion(int socket){
 	int* tamanioValorStatus = malloc(sizeof(int));
 	char* valorStatus;
 	char* valor;
-	int* entradasLibresCoordinador;
 
 	if(recv(socket, buffer_header, sizeof(header_t), MSG_WAITALL)< 0){
 		_exit_with_error(socket, ANSI_COLOR_BOLDRED"No se pudo recibir el Header"ANSI_COLOR_RESET);
 	}
 
+	bufferClave = malloc(buffer_header->tamanioClave);
+	tamanioValor = malloc(sizeof(int32_t));
+
 	switch(buffer_header->codigoOperacion){
 	case 1: /* SET */
 		/* Recibo clave y valor */
-		bufferClave = malloc(buffer_header->tamanioClave);
-		tamanioValor = malloc(sizeof(int32_t));
-
 		recibirClave(socket, buffer_header, bufferClave);
 		recibirTamanioValor(socket, tamanioValor);
 		bufferValor = malloc(*tamanioValor);
@@ -153,13 +143,11 @@ void recibirInstruccion(int socket){
 		break;
 	case 2: /* STORE */
 		/* Recibo clave */
-		bufferClave = malloc(buffer_header->tamanioClave);
 		recibirClave(socket, buffer_header, bufferClave);
 
 		store(bufferClave);
 		break;
 	case 3: /* Status */
-		bufferClave = malloc(buffer_header->tamanioClave);
 		recibirClave(socket, buffer_header, bufferClave);
 
 		if((entrada = buscarEnTablaDeEntradas(bufferClave)) != NULL && (valor = buscarEnStorage(entrada->numero)) != NULL){
@@ -178,15 +166,6 @@ void recibirInstruccion(int socket){
 				log_info(logger, "Se envio el tamanio del valor al coordinador");
 		}
 
-		break;
-	case 5: /* NOTIFICAR ENTRADAS LIBRES */
-		entradasLibresCoordinador = malloc(sizeof(int));
-		*entradasLibresCoordinador = entradasLibres();
-		if (send(socket, entradasLibres, sizeof(int), 0) < 0) {
-			_exit_with_error(socket, ANSI_COLOR_BOLDRED"No se pudo enviar la cantidad de entradas libres"ANSI_COLOR_RESET);
-		}
-
-		free(entradasLibresCoordinador);
 		break;
 	default:
 		_exit_with_error(socket, ANSI_COLOR_BOLDRED"No existe el codigo de operacion"ANSI_COLOR_RESET);
@@ -208,19 +187,6 @@ void enviarValor(int socket, int tamanioValor, char* valor){
 	if (send(socket, valor, tamanioValor, 0) < 0) {
 		_exit_with_error(socket, ANSI_COLOR_BOLDRED"No se pudo enviar el valor"ANSI_COLOR_RESET);
 	}
-}
-
-int entradasLibres() {
-	int contador = 0;
-
-	for (int i=0; i<CANTIDADENTRADAS; i++) {
-		storage = buscarEnStorage(i);
-		if (strcmp(storage, "") == 0) {
-			contador++;
-		}
-	}
-
-	return contador;
 }
 
 void set(char* clave, char* valor){
